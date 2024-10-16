2c2d3d59b9bf58e1d6086b49cc51f11a481a8a40
BenediktHC <benedikt.steinbacher@haeger-consulting.de>
2024-10-16 11:35:45 +0200

    Removed temp and 5 sec updates, added inxi command

--- 01_linux/task06_sysmonitoring/sysmonitoring.sh
+++ 01_linux/task06_sysmonitoring/sysmonitoring.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
-# Task 6 - System Monitoring
 
-# CPU Monitoring
-zones=(
-	"/sys/class/thermal/thermal_zone4/temp"
-	"/sys/class/thermal/thermal_zone5/temp"
-	"/sys/class/thermal/thermal_zone7/temp"
-)
+echo "=== CPU Usage ==="
+mpstat
 
-while true; do
-	echo "----------------------------"
-	echo "CPU TEMP"
-	for zone in "${zones[@]}"; do
-		temp=$(cat $zone)
-		temp_c=$(echo "scale=1; $temp / 1000" | bc)
-		echo "$zone: $temp_c°C"
-	done
-	echo "----------------------------"
-	echo "CPU USAGE"
-	mpstat
-	echo "............................"
-	echo "MEMORY USAGE"
-	free -h
-	echo "----------------------------"
-	echo "CONNECTED HARDWARE"
-	lsusb
-	echo "----------------------------"
-	sleep 5
-done
+echo "=== Memory Usage ==="
+inxi -m | grep -E "Used|total"
+
+echo "=== Disk Usage ==="
+inxi -D | grep "Local Storage"
+
+echo "=== Network Devices ==="
+inxi -n | grep "Device"
+
+echo "=== USB Devices ==="
+inxi -A | grep "Device"
+
+echo "=== Graphics Information ==="
+inxi -G | grep -E "Device|driver"
+
+echo "=== Connected Hardware ==="
+inxi -Fxz | grep -E "Device|Model"
 
 
