0006a37af2bb138224efd3229df69fd98c7a7123
BenediktHC <benedikt.steinbacher@haeger-consulting.de>
2024-11-06 09:04:48 +0100

    Multistage task 6

---
+++ 02_docker/multistage/task06/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('actionButton');
+    
+    button.addEventListener('click', () => {
+        alert('Button Clicked! Thanks for interacting.');
+    });
+});
